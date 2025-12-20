7e99cbb5f1fbc3aae95dcdcd5c0ab9a0d772afbb
OleksandrMoldovan <alexmoldovan1027@gmail.com>
2025-12-20 16:18:47 +0200

    Added header component

---
+++ components/Header.ts
@@ -0,0 +1,23 @@
+import { Locator, Page } from '@playwright/test'
+export class Header{
+  page: Page
+  
+  header: Locator
+  home: Locator
+  categories: Locator
+  contact: Locator
+  signIn: Locator
+  languageSwitcher: Locator
+
+  
+  constructor(page: Page) {
+    this.page = page
+    this.header = this.page.locator('app-header')
+    this.home = this.page.locator('li.nav-item [data-test="nav-home"]')
+    this.categories = this.page.locator('li.nav-item [data-test="nav-categories"]')
+    this.contact = this.page.locator('li.nav-item [data-test="nav-contact"]')
+    this.signIn = this.page.locator('li.nav-item [data-test="nav-sign-in"]')
+    this.languageSwitcher = this.page.locator('.btn-group dropdown')
+  }
+
+}
